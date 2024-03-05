3dbf1f123add533f0367b255e537525b2ea2f57e
this.gerardochavez <info.gerardo.chavez@gmail.com>
2024-03-04 21:29:08 -0300

    small change on alerts

--- src/test/java/LocatorsAndFunctions/Basic/ManejoDeAlertas.java
+++ src/test/java/LocatorsAndFunctions/Basic/ManejoDeAlertas.java
@@ -1,6 +1,5 @@
 package LocatorsAndFunctions.Basic;
 
-import java.io.IOException;
 import java.time.Duration;
 
 import org.openqa.selenium.Alert;
@@ -22,7 +21,7 @@ import com.github.javafaker.Faker;
 public class ManejoDeAlertas {
 	WebDriver driver;
 	Faker faker;
-	String url = "https://demoqa.com/alerts";
+	String url = "https://seleniumjavalocators.neocities.org/pages/alerts";
 	@BeforeMethod
 	public void setUp() {
 		driver = new ChromeDriver();
@@ -35,84 +34,84 @@ public class ManejoDeAlertas {
 	public void alertTest() {
 		WebElement alertButton = driver.findElement(By.id("alertButton"));
 		alertButton.click();
-		
+
 		Alert alert = driver.switchTo().alert();
 		System.out.println(alert.getText());
-		
+
 		Assert.assertEquals(alert.getText(), "You clicked a button");
-		
+
 		alert.accept();
-		
+
 	}
 	@Test
 	public void alertDelayedTest() {
 		WebElement alertButton = driver.findElement(By.id("timerAlertButton"));
 		alertButton.click();
-		
-	    Wait<WebDriver> waitAlert = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+		Wait<WebDriver> waitAlert = new WebDriverWait(driver, Duration.ofSeconds(10));
 		waitAlert.until(ExpectedConditions.alertIsPresent());
-	    
+
 		Alert alert = driver.switchTo().alert();
-		
+
 		System.out.println(alert.getText());
 		Assert.assertEquals(alert.getText(), "This alert appeared after 5 seconds");
-		
+
 		alert.accept();
-		
+
 	}
 	@Test
 	public void alertConfirmTest() {
 		WebElement alertButton = driver.findElement(By.id("confirmButton"));
 		alertButton.click();
-		
+
 		Alert alert = driver.switchTo().alert();
 		System.out.println(alert.getText());
-		
-		Assert.assertEquals(alert.getText(), "Do you confirm action?");
-		
+
+		Assert.assertEquals(alert.getText(), "Do you want to proceed?");
+
 		alert.accept();
-		
-		WebElement result = driver.findElement(By.id("confirmResult"));
+
+		WebElement result = driver.findElement(By.id("resultContainerConfirmButton"));
 		System.out.println(result.getText());
-		
-		Assert.assertEquals(result.getText(), "You selected Ok");
-		
+
+		Assert.assertEquals(result.getText(), "You selected OK");
+
 	}
 	@Test
 	public void alertDismissTest() {
 		WebElement alertButton = driver.findElement(By.id("confirmButton"));
 		alertButton.click();
-		
+
 		Alert alert = driver.switchTo().alert();
 		alert.dismiss();
-		
-		WebElement result = driver.findElement(By.id("confirmResult"));
+
+		WebElement result = driver.findElement(By.id("resultContainerConfirmButton"));
 		System.out.println(result.getText());
-		
+
 		Assert.assertEquals(result.getText(), "You selected Cancel");
-		
+
 	}
 	@Test
 	public void alertPromptTest() {
 		faker = new Faker();
 		String name = faker.name().firstName();
-		
-		WebElement alertButton = driver.findElement(By.id("promtButton"));
+
+		WebElement alertButton = driver.findElement(By.id("promptButton"));
 		alertButton.click();
-		
+
 		Alert alert = driver.switchTo().alert();
-		
+
 		alert.sendKeys(name);
 		alert.accept();
-		
-		WebElement result = driver.findElement(By.id("promptResult"));
+
+		WebElement result = driver.findElement(By.id("resultContainerPromptButton"));
 		System.out.println(result.getText());
-		
-		Assert.assertEquals(result.getText(), "You entered " + name);
-		
+
+		Assert.assertEquals(result.getText(), "You entered '" + name + "'");
+
 	}
 	@AfterMethod
-	public void finTest() throws IOException {
+	public void finTest() {
 		driver.close();
 		
 	}
